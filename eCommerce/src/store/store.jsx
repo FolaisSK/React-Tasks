25a4b8c2547939159cde14b55b779407bafb63c6
Fola <folajimibabslawal@gmail.com>
2026-04-07 16:50:05 +0100

    qed

--- eCommerce/src/store/store.jsx
+++ eCommerce/src/store/store.jsx
@@ -1,7 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {productApi} from "../apis/productApi.jsx";
 
 export const store = configureStore({
     reducer: {
-
-    }
+        [productApi.reducerPath]: productApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => (
+        getDefaultMiddleware.concat(productApi.middleware)
+    )
 })
